ac1cd8b0313af7c3bfc480da71970ead45931b79
Alex <83047121+GiedraAlexandra19@users.noreply.github.com>
2024-01-06 23:35:59 -0500

    BlurKernel.cu

--- LABcuda/BlurKernel.cu
+++ LABcuda/BlurKernel.cu
@@ -10,6 +10,27 @@
 #define BLUR_SIZE 4
 using namespace cv;
 __global__
+void blur_grayKernel(unsigned char* in, unsigned char* out, int w, int h) {
+    int Col = blockIdx.x * blockDim.x + threadIdx.x;
+    int Row = blockIdx.y * blockDim.y + threadIdx.y;
+    if (Col < w && Row < h) {
+        int pixVal = 0;
+        int pixels = 0;
+        for (int blurRow = -BLUR_SIZE; blurRow < BLUR_SIZE + 1; ++blurRow) {
+            for (int blurCol = -BLUR_SIZE; blurCol < BLUR_SIZE + 1; ++blurCol) {
+                int curRow = Row + blurRow;
+                int curCol = Col + blurCol;
+                if (curRow > -1 && curRow < h && curCol > -1 && curCol < w) {
+                    pixVal += in[curRow * w + curCol];
+                    pixels++;
+                }
+            }
+        }
+        out[Row * w + Col] = (unsigned char)(pixVal / pixels);
+    }
+}
+
+__global__
 void blurKernel(unsigned char* in, unsigned char* out, int w, int h) {
     int Col = blockIdx.x * blockDim.x + threadIdx.x;
     int Row = blockIdx.y * blockDim.y + threadIdx.y;
@@ -35,7 +56,7 @@ void blurKernel(unsigned char* in, unsigned char* out, int w, int h) {
 }
 int main()
 {
-    cv::Mat img = cv::imread("D:/fmatt/Pictures/Wallpapers/LenovoWallPaper.jpg");
+    cv::Mat img = cv::imread("original.jpg");
     // Get image dimensions
     int width = img.cols;
     int height = img.rows;
@@ -60,6 +81,7 @@ int main()
 
     cv::imshow("Input Image", img);
     cv::imshow("Blurred Image", outputImage);
+    cv::imwrite("blur.jpg", outputImage);
     cv::waitKey(0);
 
     return 0;
